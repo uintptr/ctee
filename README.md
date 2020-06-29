da855193d17484d9b0c762e60789b9ccd9920ab9
fsck <inaforloop@gmail.com>
2020-06-29 09:49:30 -0400

    ctee: Don't break if the config file isn't there

---
+++ README.md
@@ -0,0 +1,16 @@
+Colored Tee. Ideally it'll work like `tee`.
+
+## Config file
+* Copy `ctee.conf` to `~/.ctee.conf`
+
+## How to
+
+```
+adb logcat | /path/to/ctee.py
+```
+
+And simply (if `ctee` in somewhere in `$PATH` )
+
+```
+adb logcat | /path/to/ctee.py
+```
--- ctee.py
+++ ctee.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import os
@@ -7,53 +7,56 @@ import re
 
 BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, NONE, DROP = range(10)
 
-colors = [  "BLACK",
-            "RED",
-            "GREEN",
-            "YELLOW",
-            "BLUE",
-            "MAGENTA",
-            "CYAN",
-            "WHITE",
-            "NONE",
-            "DROP" ]
+colors = ["BLACK",
+          "RED",
+          "GREEN",
+          "YELLOW",
+          "BLUE",
+          "MAGENTA",
+          "CYAN",
+          "WHITE",
+          "NONE",
+          "DROP"]
 
-def console_print ( text, color=WHITE ):
-    text = "\x1b[1;%dm" % ( 30 + color ) + text + "\x1b[0m"
-    print text
 
-def parse_config ( conf_file ):
+def console_print(text, color=WHITE):
+    text = "\x1b[1;%dm" % (30 + color) + text + "\x1b[0m"
+    print(text)
 
-    strings   = []
+
+def parse_config(conf_file):
+
+    strings = []
     color_map = {}
 
-    print "[+] parsing {}".format ( conf_file )
+    print("[+] parsing {}".format(conf_file))
 
-    if ( True == os.path.isfile ( conf_file ) ):
-        with open ( conf_file, "r" ) as f:
+    if (True == os.path.isfile(conf_file)):
+        with open(conf_file, "r") as f:
             for line in f:
                 line = line.strip("\r\n")
-                if( True == line.startswith ( '#' ) or
-                    0    == len ( line ) ):
+                if(True == line.startswith('#') or
+                        0 == len(line)):
                     continue
-                (s,c) = line.split('=')
+                (s, c) = line.split('=')
 
-                color  = c.strip(' ')
-                string = re.sub ( '" *', '', s )
+                color = c.strip(' ')
+                string = re.sub('" *', '', s)
 
-                strings.append ( string )
+                strings.append(string)
 
-                color_map[string] = colors.index ( color )
+                color_map[string] = colors.index(color)
 
-    return (strings, color_map )
+    return (strings, color_map)
 
-def read_loop( out_fd ):
 
-    conf_file = os.path.expanduser ( "~/.ctee.conf" )
+def read_loop(out_fd):
 
-    if ( True == os.path.exists ( conf_file ) ):
-        config_ts = os.stat ( conf_file ).st_mtime
-        strings, color_map = parse_config( conf_file )
+    conf_file = os.path.expanduser("~/.ctee.conf")
+
+    if (True == os.path.exists(conf_file)):
+        config_ts = os.stat(conf_file).st_mtime
+        strings, color_map = parse_config(conf_file)
     else:
         config_ts = 0
         strings = []
@@ -63,7 +66,7 @@ def read_loop( out_fd ):
 
         line = sys.stdin.readline()
 
-        if ( None == line or 0 == len ( line ) ):
+        if (None == line or 0 == len(line)):
             #
             # stdint died ?
             #
@@ -75,36 +78,37 @@ def read_loop( out_fd ):
         # see if the config file changed and reparse it so the user
         # doesn't have to interrupt his thing
         #
-        if ( config_ts != os.stat ( conf_file ).st_mtime ):
-            config_ts = os.stat ( conf_file ).st_mtime
-            strings, color_map = parse_config( conf_file )
+        if (True == os.path.exists(conf_file)):
+            if (config_ts != os.stat(conf_file).st_mtime):
+                config_ts = os.stat(conf_file).st_mtime
+                strings, color_map = parse_config(conf_file)
 
         color = NONE
 
         for s in strings:
-            if None != re.search ( s, line ):
+            if None != re.search(s, line):
                 color = color_map[s]
                 break
 
-        if ( DROP == color ):
+        if (DROP == color):
             continue
 
-        if ( None != out_fd ):
-            out_fd.write ( line + "\n" )
+        if (None != out_fd):
+            out_fd.write(line + "\n")
             out_fd.flush()
 
-        if ( NONE == color ):
-            print line
+        if (NONE == color):
+            print(line)
         else:
-            console_print ( line, color )
+            console_print(line, color)
+
 
 def main():
 
-
-    if ( 1 == len ( sys.argv ) ):
+    if (1 == len(sys.argv)):
         read_loop(None)
     else:
-        with open ( sys.argv[1], "a" ) as f:
+        with open(sys.argv[1], "a") as f:
             read_loop(f)
 
 
