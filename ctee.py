dd96c9e9bc9cc3b03f9423164a9a34a77498fdbb
fsck <inaforloop@gmail.com>
2023-11-29 19:51:24 -0500

    catch KeyboardInterrupt

--- ctee.py
+++ ctee.py
@@ -54,11 +54,18 @@ def parse_config(conf_file: str) -> tuple[list[str], dict[str, int]]:
 
 def read_loop(out_fd: Optional[io.IOBase] = None):
 
-    conf_file = os.path.expanduser("~/.ctee.conf")
+    config_file = None
+    home_conf_file = os.path.expanduser("~/.ctee.conf")
+    sxs_conf_filr = os.path.abspath("ctee.conf")
 
-    if (True == os.path.exists(conf_file)):
-        config_ts = os.stat(conf_file).st_mtime
-        strings, color_map = parse_config(conf_file)
+    if (True == os.path.exists(home_conf_file)):
+        config_file = home_conf_file
+    elif (True == os.path.exists(sxs_conf_filr)):
+        config_file = sxs_conf_filr
+
+    if (config_file is not None):
+        config_ts = os.stat(config_file).st_mtime
+        strings, color_map = parse_config(config_file)
     else:
         config_ts = 0
         strings = []
@@ -80,10 +87,10 @@ def read_loop(out_fd: Optional[io.IOBase] = None):
         # see if the config file changed and reparse it so the user
         # doesn't have to interrupt his thing
         #
-        if (True == os.path.exists(conf_file)):
-            if (config_ts != os.stat(conf_file).st_mtime):
-                config_ts = os.stat(conf_file).st_mtime
-                strings, color_map = parse_config(conf_file)
+        if(config_file is not None):
+            if (config_ts != os.stat(config_file).st_mtime):
+                config_ts = os.stat(config_file).st_mtime
+                strings, color_map = parse_config(config_file)
 
         color = NONE
 
@@ -105,14 +112,26 @@ def read_loop(out_fd: Optional[io.IOBase] = None):
             console_print(line, color)
 
 
-def main():
+def main() -> int:
 
-    if (1 == len(sys.argv)):
-        read_loop()
-    else:
-        with open(sys.argv[1], "a") as f:
-            read_loop(f)
+    status = 1
+
+    try:
+        if (1 == len(sys.argv)):
+            read_loop()
+        else:
+            with open(sys.argv[1], "a") as f:
+                read_loop(f)
+        status = 0
+    except KeyboardInterrupt:
+        status = 0
+
+    return status
 
 
 if __name__ == '__main__':
-    sys.exit(main())
+
+    status = main()
+
+    if (0 != status):
+        sys.exit(status)
